cd5cca0eeb8b38d2cb735ef1b41cda993a8eac67
Hawkeye2108 <jatinrawat2108@gmail.com>
2024-09-08 14:30:12 +0530

    Second Commit

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const server = http.createServer(app);
 
 const io = socketIo(server,{
     cors:{
-        origin:"http://localhost:5173"
+        origin:"*"
     }
 })
 app.use(cors({
-   origin:"http://localhost:5173"
+   origin:"*"
 }));
 
 // Handle socket connections
